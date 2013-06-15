47dffbd3ac71029debf8bde6512fca8bb17872cb
manymengofishing <mattheyj@googlemail.com>
2013-06-15 16:51:42 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whiskyscience::Application.config.secret_token = 'c691988bf6816631ac6edab8dd4f34f70e7249c70ba3fc6b6dadf5a3801f69cfbd0e4236613cea7dac4497ebf0b0bcf6c96963a2f7d56a7c0e7f079be9ef0c29'
